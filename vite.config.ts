a1cfa0b7a00ecc942486e3b4cdf435db3af2bb1c
Geethika K <geethika.kotramangalam@techdenali.com>
2026-06-19 18:20:16 +0530

    Initial project upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.widget.tsx"),
+      name: "CaasWidget",
+      formats: ["iife"],
+      fileName: () => "demobundle.js",
+    },
+    rollupOptions: {
+      external: [],
+    },
+    minify: true,
+  },
+  define: {
+    // Replace process.env with empty object
+    "process.env": {},
+  },
+});
