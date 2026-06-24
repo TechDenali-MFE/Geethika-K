56e6eecc6c4c98b5edf125a2098cd9825bc2f9d5
Geethika K <geethika.kotramangalam@techdenali.com>
2026-06-24 15:28:52 +0530

    tailwind added h1

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App: React.FC<AppProps> = ({ options }: any) => {
       <h2 className="text-2xl font-bold text-blue-600 mb-4">
         This is My Widget
       </h2>
-
+<h1>hlooo</h1>
       <button
         onClick={handleIncrease}
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
