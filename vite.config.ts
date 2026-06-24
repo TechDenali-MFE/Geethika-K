b374dabdc1ed9c0c85b75cc79ff274ccaa0af438
Geethika K <geethika.kotramangalam@techdenali.com>
2026-06-24 18:37:45 +0530

    tailwind plugin added

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
+      cssInjectedByJsPlugin(),
   ],
   build: {
     lib: {
