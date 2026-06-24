15fa8e0c21b1c45d936e8459430b6d1782ba22c5
Geethika K <geethika.kotramangalam@techdenali.com>
2026-06-24 18:40:33 +0530

    tailwind plugin added changed color

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC<AppProps> = ({ options }: any) => {
 <h1>hlooo</h1>
       <button
         onClick={handleIncrease}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
       >
         Increase Counter
       </button>
